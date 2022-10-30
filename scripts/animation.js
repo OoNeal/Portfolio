3a35ada53c2394b0a6b7c9e422e45d4eccd94202
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-10-30 13:32:12 +0100

    ⚡New script for an animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,14 +1,28 @@
 window.addEventListener('scroll', function (e) {
-    const elements = document.getElementsByClassName('animation');
+    const elements = document.getElementsByClassName('div-animation');
 
-    for (let i = 0; i < elements.length; i++) {
+    let passed = 0;
+    for (let i = passed; i < elements.length; i++) {
         const element = elements[i];
         const position = element.getBoundingClientRect();
 
-        if (position.top < window.innerHeight) {
-            element.style.animationPlayState = 'running';
-            break;
+        if (position.top <= window.innerHeight - position.height / 2 && position.bottom >= position.height / 2) {
+            element.classList.remove('div-animation');
+            element.classList.add('div-animation_active');
+            passed++;
         }
     }
 });
 
+window.addEventListener('mousemove', function (f) {
+    // const images equal all images
+    const images = document.querySelectorAll('img');
+
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+
+        //transform image to mouse position in relation to the middle of the screen
+        image.style.transform = 'rotate(' + (f.clientX - window.innerWidth / 2) / 200 + 'deg';
+        image.style.transform += 'translate( 0,' + (f.clientY - window.innerHeight / 2) / 200 + 'px';
+    }
+});
